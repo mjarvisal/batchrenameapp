f2a4719d0aed6bacaa1edaed7226b117ac7c84ac
Reaby <petri.jarvisalo@gmail.com>
2018-06-16 17:43:50 +0300

    nearly there!

--- BatchRenameApp/MainWindow.Designer.cs
+++ BatchRenameApp/MainWindow.Designer.cs
@@ -30,62 +30,142 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.listBoxFilelist = new System.Windows.Forms.ListBox();
-            this.textBoxRegExp = new System.Windows.Forms.TextBox();
+            this.inputSearch = new System.Windows.Forms.TextBox();
             this.labelRegExp = new System.Windows.Forms.Label();
             this.labelFileList = new System.Windows.Forms.Label();
+            this.labelReplace = new System.Windows.Forms.Label();
+            this.inputReplace = new System.Windows.Forms.TextBox();
+            this.buttonPreview = new System.Windows.Forms.Button();
+            this.listBoxPreview = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRename = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBoxFilelist
             // 
+            this.listBoxFilelist.AllowDrop = true;
             this.listBoxFilelist.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBoxFilelist.FormattingEnabled = true;
             this.listBoxFilelist.HorizontalScrollbar = true;
-            this.listBoxFilelist.Location = new System.Drawing.Point(270, 38);
+            this.listBoxFilelist.Location = new System.Drawing.Point(12, 140);
             this.listBoxFilelist.Name = "listBoxFilelist";
             this.listBoxFilelist.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.listBoxFilelist.Size = new System.Drawing.Size(197, 459);
+            this.listBoxFilelist.Size = new System.Drawing.Size(197, 316);
             this.listBoxFilelist.TabIndex = 0;
+            this.listBoxFilelist.SelectedIndexChanged += new System.EventHandler(this.listBoxFilelist_SelectedIndexChanged);
+            this.listBoxFilelist.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxFilelist_DragDrop);
+            this.listBoxFilelist.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFilelist_DragEnter);
+            this.listBoxFilelist.DoubleClick += new System.EventHandler(this.listBoxFilelist_DoubleClick);
             // 
-            // textBoxRegExp
+            // inputSearch
             // 
-            this.textBoxRegExp.Location = new System.Drawing.Point(12, 78);
-            this.textBoxRegExp.Name = "textBoxRegExp";
-            this.textBoxRegExp.Size = new System.Drawing.Size(252, 20);
-            this.textBoxRegExp.TabIndex = 1;
+            this.inputSearch.Location = new System.Drawing.Point(12, 38);
+            this.inputSearch.Multiline = true;
+            this.inputSearch.Name = "inputSearch";
+            this.inputSearch.Size = new System.Drawing.Size(252, 20);
+            this.inputSearch.TabIndex = 1;
             // 
             // labelRegExp
             // 
             this.labelRegExp.AutoSize = true;
-            this.labelRegExp.Location = new System.Drawing.Point(13, 59);
+            this.labelRegExp.Location = new System.Drawing.Point(12, 22);
             this.labelRegExp.Name = "labelRegExp";
-            this.labelRegExp.Size = new System.Drawing.Size(92, 13);
+            this.labelRegExp.Size = new System.Drawing.Size(41, 13);
             this.labelRegExp.TabIndex = 2;
-            this.labelRegExp.Text = "regular expression";
+            this.labelRegExp.Text = "Search";
             // 
             // labelFileList
             // 
             this.labelFileList.AutoSize = true;
-            this.labelFileList.Location = new System.Drawing.Point(267, 22);
+            this.labelFileList.Location = new System.Drawing.Point(12, 124);
             this.labelFileList.Name = "labelFileList";
-            this.labelFileList.Size = new System.Drawing.Size(35, 13);
+            this.labelFileList.Size = new System.Drawing.Size(89, 13);
             this.labelFileList.TabIndex = 3;
-            this.labelFileList.Text = "Filelist";
+            this.labelFileList.Text = "Original filenames";
+            // 
+            // labelReplace
+            // 
+            this.labelReplace.AutoSize = true;
+            this.labelReplace.Location = new System.Drawing.Point(12, 67);
+            this.labelReplace.Name = "labelReplace";
+            this.labelReplace.Size = new System.Drawing.Size(47, 13);
+            this.labelReplace.TabIndex = 5;
+            this.labelReplace.Text = "Replace";
+            // 
+            // inputReplace
+            // 
+            this.inputReplace.Location = new System.Drawing.Point(12, 83);
+            this.inputReplace.Name = "inputReplace";
+            this.inputReplace.Size = new System.Drawing.Size(252, 20);
+            this.inputReplace.TabIndex = 4;
+            // 
+            // buttonPreview
+            // 
+            this.buttonPreview.Location = new System.Drawing.Point(286, 80);
+            this.buttonPreview.Name = "buttonPreview";
+            this.buttonPreview.Size = new System.Drawing.Size(75, 23);
+            this.buttonPreview.TabIndex = 6;
+            this.buttonPreview.Text = "Preview";
+            this.buttonPreview.UseVisualStyleBackColor = true;
+            this.buttonPreview.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBoxPreview
+            // 
+            this.listBoxPreview.AllowDrop = true;
+            this.listBoxPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPreview.FormattingEnabled = true;
+            this.listBoxPreview.HorizontalScrollbar = true;
+            this.listBoxPreview.Location = new System.Drawing.Point(267, 140);
+            this.listBoxPreview.Name = "listBoxPreview";
+            this.listBoxPreview.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxPreview.Size = new System.Drawing.Size(197, 316);
+            this.listBoxPreview.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Changed filenames";
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Location = new System.Drawing.Point(389, 462);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(75, 23);
+            this.buttonRename.TabIndex = 9;
+            this.buttonRename.Text = "Rename!";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
             // 
             // MainWindow
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 511);
+            this.ClientSize = new System.Drawing.Size(487, 516);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxPreview);
+            this.Controls.Add(this.buttonPreview);
+            this.Controls.Add(this.labelReplace);
+            this.Controls.Add(this.inputReplace);
             this.Controls.Add(this.labelFileList);
             this.Controls.Add(this.labelRegExp);
-            this.Controls.Add(this.textBoxRegExp);
+            this.Controls.Add(this.inputSearch);
             this.Controls.Add(this.listBoxFilelist);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(495, 550);
             this.Name = "MainWindow";
             this.Text = "BatchRenameApp";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxFilelist_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFilelist_DragEnter);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,9 +174,15 @@
         #endregion
 
         public System.Windows.Forms.ListBox listBoxFilelist;
-        private System.Windows.Forms.TextBox textBoxRegExp;
+        private System.Windows.Forms.TextBox inputSearch;
         private System.Windows.Forms.Label labelRegExp;
+        private System.Windows.Forms.Label labelReplace;
+        private System.Windows.Forms.TextBox inputReplace;
+        private System.Windows.Forms.Button buttonPreview;
+        public System.Windows.Forms.ListBox listBoxPreview;
         private System.Windows.Forms.Label labelFileList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRename;
     }
 }
 
--- BatchRenameApp/MainWindow.cs
+++ BatchRenameApp/MainWindow.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,22 +9,161 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace BatchRenameApp
 {
     public partial class MainWindow : Form
     {
-        string[] args;
+        string[] args; 
+        HashSet<FileInfo> files = new HashSet<FileInfo>();
+
+
         public MainWindow()
         {
             InitializeComponent();
-            listBoxFilelist.Items.AddRange(GetArgs());
+            files.Clear();
+            int errors = 0;
+            Exception outException = null;
+
+           foreach (string item in GetArgs())
+            {
+                try
+                {
+                    AddFile(item);
+                } catch (Exception e)
+                {
+                    errors += 1;
+                    outException = e;
+                }
+            }
+
+            if (errors > 0)
+            {
+                MessageBox.Show(outException.Message, errors + "Errors", MessageBoxButtons.OK);
+            }
+
+
         }
+
         private string[] GetArgs()
         {
             args = Environment.GetCommandLineArgs();
             args = args.Skip(1).ToArray();
             return args;
+        }
+
+        private void AddFile(string filename)
+        {
+            FileInfo file = new FileInfo(filename);
+            
+            // check for duplicates
+            foreach (FileInfo tmpFile in files)
+            {
+                if (tmpFile.FullName == file.FullName)
+                {
+                    return;
+                }
+            }
+            // if no duplicates, add file to list
+
+           files.Add(file);
+           listBoxFilelist.Items.Add(file);
+        }
+
+        private void RemoveFile(string filename)
+        {
+            FileInfo file = new FileInfo(filename);
+            int x = 0;
+            foreach (FileInfo item in listBoxFilelist.Items)
+            {
+                if (file.FullName == item.FullName)
+                {
+                    listBoxFilelist.Items.Remove(item);
+                    files.Remove(item);
+                    return;
+                }
+                x += 1;
+            }
+        }
+
+        private void listBoxFilelist_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+
+        }
+
+        private void listBoxFilelist_DragDrop(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in files)
+            {
+                AddFile(file);
+            }
+        }
+
+        private void listBoxFilelist_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listBoxFilelist_DoubleClick(object sender, EventArgs e)
+        {
+            ListBox listbox = (ListBox)sender;
+
+            if (listbox.SelectedItem != null)
+            {
+                RemoveFile(listbox.SelectedItem.ToString());
+                UpdatePreview();
+            }
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdatePreview();
+        }
+
+
+        private void UpdatePreview()
+        {
+            listBoxPreview.Items.Clear();
+
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+                    Regex regex = new Regex(inputSearch.Text);
+                    listBoxPreview.Items.Add(file.Directory + "\\" + regex.Replace(file.Name, inputReplace.Text));
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            if ( listBoxPreview.Items.Count == 0) {
+                MessageBox.Show("You must preview changes before renaming! ", "Error", MessageBoxButtons.OK);
+                    return;
+            }
+
+            if (listBoxPreview.Items.Count != listBoxFilelist.Items.Count)
+            {
+                MessageBox.Show("File counts differ, might be regex error", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            // @todo - do actual renaming!
+
+
+
         }
     }
 }
