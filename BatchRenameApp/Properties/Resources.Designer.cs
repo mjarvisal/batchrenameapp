9fce852dc11ac73eddfe75b9c3922bcbaf38bea6
Markus Järvisalo <markus.jarvisalo@gmail.com>
2018-06-23 14:39:54 +0300

    [Improvement] Got rid of CollapsibleGroupBox.dll

--- BatchRenameApp/Properties/Resources.Designer.cs
+++ BatchRenameApp/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace BatchRenameApp.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap minus {
+            get {
+                object obj = ResourceManager.GetObject("minus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap plus {
+            get {
+                object obj = ResourceManager.GetObject("plus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
