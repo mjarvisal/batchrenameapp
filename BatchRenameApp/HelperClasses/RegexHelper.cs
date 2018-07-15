5a92bd29a8059a6997d6339f7c5e2ab2bc32a61d
Reaby <petri.jarvisalo@gmail.com>
2018-07-15 12:34:00 +0300

    add missing file

---
+++ BatchRenameApp/HelperClasses/RegexHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace BatchRenameApp
+{
+    public class RegexHelper
+    {
+        private Regex regex;
+        public bool bIsValidRegex = false;
+
+        public RegexHelper(string Filter)
+        {
+            try
+            {
+                regex = new Regex(Filter);
+                bIsValidRegex = true;
+            }
+            catch
+            {
+                regex = new Regex("^");
+                bIsValidRegex = false;
+            }
+        }
+
+        public Regex GetRegex()
+        {
+            return regex;
+        }
+
+
+        public MatchCollection getMatches(string Text)
+        {
+            return regex.Matches(Text);
+        }
+
+    }
+}
